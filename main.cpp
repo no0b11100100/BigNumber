0f1d5b7c10c589d1d17097bab2441bf2b78dcc55
drago <fedorovskiy000@gmail.com>
2021-02-24 22:24:27 +0200

    add Timer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
-#include "bignumber.hpp"
+//#include "bignumber.hpp"
+#include "Operators.h"
+#include <chrono>
 
 using namespace std;
 
-int main()
+namespace Timer
 {
 
+using ns = std::chrono::nanoseconds;
+using mcs = std::chrono::microseconds;
+using ms = std::chrono::milliseconds;
+using s = std::chrono::seconds;
+
+template <class TimeMeasurement>
+class Timer
+{
+public:
+
+    template<class Callable, class ...Args>
+    Timer(Callable&& callable, Args&& ... args)
+    {
+        auto startTime = std::chrono::system_clock::now();
+        std::invoke(callable, std::forward<Args>(args)...);
+        auto endTime = std::chrono::system_clock::now();
+        m_time = std::chrono::duration_cast<TimeMeasurement>(endTime - startTime).count();
+    }
+
+    double Time() const { return m_time; }
+
+private:
+    double m_time;
+};
+
+} // Timer
+
+int main()
+{
+    BinaryData a {1,0,0,0,0,0};
+    BinaryData b {1,1};
+
+    Timer::Timer<Timer::mcs> t(division, a, b);
+
+    cout << "execution time is " << t.Time() << endl;
+
+    return 0;
 }
